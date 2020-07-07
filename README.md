74410161e0901a335533c34d7e896f2e840b86c2
Mircea Nagy <mircea@appniv.com>
2020-07-07 17:15:25 +0300

    Working JS browser-side media producer and media consumer

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 Streams a screenshare into KVS using the PutMedia API from the JS client
 
 ### Build and run
-npm install 
-npm run build 
-chrome.exe --user-data-dir="C://Chrome dev session" --disable-web-security
+- npm install 
+- npm run build 
+- chrome.exe --user-data-dir="C://Chrome dev session" --disable-web-security
 
--- index.js
+++ index.js
@@ -1,14 +1,24 @@
 var aws4 = require('aws4')
 var http = require('https')
+var AWS = require('aws-sdk');
+var uuid = require('node-uuid');
 
-module.exports = {sign: getSignature, request: request};
+module.exports = {sign: getSignature, request: request, getMedia: getMedia};
 
 function getSignature(opts, accessKeyId, secretAccessKey) {
 	return aws4.sign(opts, { accessKeyId: accessKeyId, secretAccessKey: secretAccessKey });
 }
 
-function request(opts) { http.request(opts, (res) => {
-  console.log('statusCode:', res.statusCode);
-  console.log('headers:', res.headers);
-}).end(opts.body || '') }
+function request(opts, callback) { http.request(opts, callback).end(opts.body || '') }
+
+function getMedia(options, params, callback) {
+	var kinesisvideomedia = new AWS.KinesisVideoMedia({
+	'credentials': new AWS.Credentials({
+		accessKeyId: options.accessKeyId, secretAccessKey: options.secretAccessKey, sessionToken: null
+	}),
+	'region' : options.region,
+	'endpoint' : new AWS.Endpoint(options.endpoint)
+	});
+	kinesisvideomedia.getMedia(params, callback);
+}
 
